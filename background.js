1971789aa6b920f3bf6db93f44f38c79f8e5ace1
@ctac1412 <dom333122@ynamdex.ru>
2022-08-06 14:07:55 +0300

    Фикс релоуда файла

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
-  // let color = '#3aa757';
 
-  // chrome.runtime.onInstalled.addListener(() => {
-  //   chrome.storage.sync.set({ color });
-  //   console.log('Default background color set to %cgreen', `color: ${color}`);
-  // });
+function reloadData() {
+  return fetch(
+    "https://firebasestorage.googleapis.com/v0/b/everwhois.appspot.com/o/public%2Fdata.json?alt=media&token=7cfdb98c-2648-45ff-a939-8773f52d916d",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((parsed) => {
+      chrome.storage.local.set({ everwhois: parsed }, function () {
+        console.log("Value is set to ", parsed);
+      });
+      return parsed;
+    });
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  let parsed = reloadData().then(() => {
+    chrome.tabs.reload(tab.id);
+  });
+});
+
+reloadData();
