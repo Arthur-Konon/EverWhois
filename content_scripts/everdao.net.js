437da477bffb20c9bb6c64e04cd586790581beed
@ctac1412 <dom333122@ynamdex.ru>
2023-02-18 13:46:00 +0300

    Экспорт таблицы everdao

--- content_scripts/everdao.net.js
+++ content_scripts/everdao.net.js
@@ -1,4 +1,28 @@
-
+function copyToClickboard(){
+    let results = [
+        "Voter\tName\tReason\tVoting\tVoting value\tDate"
+    ];
+    document.querySelectorAll("#votes .table__body div.user-card__name a").forEach(el=>{
+        let container = el.parentNode.parentNode.parentNode.parentNode
+        let reason = container.childNodes[1].textContent;
+        let voting_type = container.childNodes[2].querySelector('.vote-type__badge');
+        let voting_count = container.childNodes[2].querySelector('.vote-type__value');
+        let date = container.childNodes[3].textContent;
+        results.push([
+            el.text,
+            el.href.replace("https://everdao.net/staking/explorer/", ""),
+            reason.replace(/\n/g, " "),
+            voting_type?voting_type.textContent:'-',
+            voting_count?voting_count.textContent:'-',
+            date,
+        ].join("\t"))
+    })
+    navigator.clipboard.writeText(results.join('\r\n')).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
 function swap(container, itemsPath, data){
     console.log('Try find   :>> ', container);
     return waitForElm(container).then((elm) => {
@@ -14,6 +38,24 @@ function swap(container, itemsPath, data){
                     item.textContent = newValue;
                 }
             }
+
+            let table_with_footer = document.querySelector("#votes > div.card.card--flat.card--small .pagination");
+            if (table_with_footer !== null && !table_with_footer.classList.contains('EverWhoisPatched')){
+                const my_div = document.createElement("div");
+                my_div.classList.add("pagination-foot")
+                const my_button = document.createElement("button");
+                my_button.addEventListener("click", copyToClickboard);
+                my_button.textContent = "Copy to clipboard";
+                my_button.classList.add("btn");
+                my_button.classList.add("btn--md");
+                my_button.classList.add("btn--inline");
+                my_button.classList.add("btn--secondary");
+                my_div.appendChild(my_button);
+                // my_div.innerHTML = `<div class="pagination-footer"><button onclick="copyToClickboard()">Скопировать</button></div>`
+                table_with_footer.insertBefore(my_div, table_with_footer.firstChild);
+                table_with_footer.classList.add("EverWhoisPatched");
+            }
+
         }
         const config = { childList: true };
         console.log('targetNode :>> ', elm);
@@ -23,6 +65,8 @@ function swap(container, itemsPath, data){
         return observer
     });
 }
+
+    
 
 const restart = (res) => {
     swap("#votes .table__body", " div.user-card__name a", res)
